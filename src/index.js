14370e0376c7b2b176ee3cf27999dae30bef1749
jordiklavers <jordiklavers@gmail.com>
2025-04-10 15:33:20 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 initNavMenu();
+initAccordionCSS();
 
 function initNavMenu() {
+  const menuToggle = document.querySelector(".nav_btn-row"); // Adjust selector if needed
+  const navLinks = document.querySelector(".nav_links");
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", () => {
+      navLinks.classList.toggle("is-open");
+    });
+  }
+
   console.log("Nav Menu");
 
   // Mobile
   if (navigator.userAgent.includes("Mobile")) {
-    console.log("mobile")
+    console.log("mobile");
     // Menu
     let menuToggle = $(".nav_mobile-btn");
     let menu = $(".nav_links");
 
     menuToggle.click(function () {
-        console.log("click")
+      console.log("click");
       menu.toggleClass("is-active");
     });
 
@@ -23,18 +33,40 @@ function initNavMenu() {
       });
     });
   } else {
-    console.log("Not 480")
+    console.log("Not 480");
   }
 }
 
-// Mobile menu toggle
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.nav_btn-row'); // Adjust selector if needed
-  const navLinks = document.querySelector('.nav_links');
+function initAccordionCSS() {
+  document
+    .querySelectorAll("[data-accordion-css-init]")
+    .forEach((accordion) => {
+      const closeSiblings =
+        accordion.getAttribute("data-accordion-close-siblings") === "true";
 
-  if (menuToggle && navLinks) {
-    menuToggle.addEventListener('click', () => {
-      navLinks.classList.toggle('is-open');
+      accordion.addEventListener("click", (event) => {
+        const toggle = event.target.closest("[data-accordion-toggle]");
+        if (!toggle) return; // Exit if the clicked element is not a toggle
+
+        const singleAccordion = toggle.closest("[data-accordion-status]");
+        if (!singleAccordion) return; // Exit if no accordion container is found
+
+        const isActive =
+          singleAccordion.getAttribute("data-accordion-status") === "active";
+        singleAccordion.setAttribute(
+          "data-accordion-status",
+          isActive ? "not-active" : "active"
+        );
+
+        // When [data-accordion-close-siblings="true"]
+        if (closeSiblings && !isActive) {
+          accordion
+            .querySelectorAll('[data-accordion-status="active"]')
+            .forEach((sibling) => {
+              if (sibling !== singleAccordion)
+                sibling.setAttribute("data-accordion-status", "not-active");
+            });
+        }
+      });
     });
-  }
-});
+}
